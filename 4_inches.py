baacdc526ba445214e3d7cd62265da3afa0b375b
Borislav Ignatov <70973921+brown223@users.noreply.github.com>
2024-10-26 20:55:22 +0300

    Add files via upload

---
+++ 4_inches.py
@@ -0,0 +1,6 @@
+inches = float(input())
+inch = 2.54
+
+print(inches * inch)
+
+# см към инчове
---
+++ 7_architect.py
@@ -0,0 +1,7 @@
+architect_name = input()
+total_projects = int(input())
+
+time_for_projects = 3
+hours_needed = total_projects * time_for_projects
+
+print(f"The architect {architect_name} will need {hours_needed} hours to complete {total_projects} project/s.")
